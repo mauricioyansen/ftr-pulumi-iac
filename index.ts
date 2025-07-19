1488d018bbfd4747c53c405b227d1c41e4b2c159
Mauricio <mauricio_cy@hotmail.com>
2025-07-18 21:22:40 -0300

    new: first commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+import * as aws from "@pulumi/aws";
+
+const bucket = new aws.s3.BucketV2("test-bucket-yang-2", {
+  bucket: "test-bucket-yang-2",
+  tags: { IAC: "true" },
+});
+
+const ecr = new aws.ecr.Repository("test-ecr-yang-2", {
+  name: "test-ecr-yang-2",
+  imageTagMutability: "IMMUTABLE",
+  tags: { IAC: "true" },
+});
+
+export const bucketName = bucket.id;
+export const bucketRegion = bucket.region;
+export const bucketArn = bucket.arn;
+
+export const ecrName = ecr.name;
+export const ecrRepositoryUrl = ecr.repositoryUrl;
